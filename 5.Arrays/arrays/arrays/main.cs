9e7aa2524d632c6ace06db833da8e79ef4c28a11
Jorge Massi <104774521+JorgeMassi@users.noreply.github.com>
2023-05-07 13:33:51 +0100

    Arrays and TicTacToe in C#

---
+++ 5.Arrays/arrays/arrays/main.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1.The application receives five numbers and returns them
+            ////ordered from lowest to largest.The numbers before
+            ////being displayed must be stored in an array;
+
+
+            //int[] arr = new int[5];
+            //Console.WriteLine("1.five numbers:");
+            //arr[0]= Convert.ToInt32(Console.ReadLine());
+            //arr[1] = Convert.ToInt32(Console.ReadLine());
+            //arr[2] = Convert.ToInt32(Console.ReadLine());
+            //arr[3] = Convert.ToInt32(Console.ReadLine());
+            //arr[4] = Convert.ToInt32(Console.ReadLine());
+
+            //Array.Sort(arr);
+            //Console.WriteLine("array:" + arr[0]);
+            //Console.WriteLine("array:" + arr[1]);
+            //Console.WriteLine("array:" + arr[2]);
+            //Console.WriteLine("array:" + arr[3]);
+            //Console.WriteLine("array:" + arr[4]);
+
+
+
+
+            ////2.The application receives five numbers and returns them
+            ////ordered from lowest to largest.The numbers must be
+            ////stored in an array as they are received;
+
+            //int j;
+            //int[] arrSecond = new int[5];
+            //Console.WriteLine("2.five numbers:");
+
+            //for (j = 0; j < 5; j++)
+            //{
+            //    arrSecond[j] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Array.Sort(arrSecond);
+            //for (j = 0; j < 5; j++)
+            //{
+            //    Console.WriteLine("array:" + arrSecond[j]);
+            //}
+
+
+            ////3.The application receives five numbers and returns the average.
+            ////The initial numbers must be stored in a array;
+
+            //int l;
+            //int[] arrTherd = new int[5];
+            //int sum = 0;
+            //float average = 0.0F;
+            //Console.WriteLine("3.five numbers:");
+            //for (l = 0; l < 5; l++)
+            //{
+            //    arrTherd[l] = Convert.ToInt32(Console.ReadLine());
+            //    sum += arrTherd[l];
+            //    average = (float)sum / 5;
+            //}
+
+            //Console.WriteLine("average:" + average);
+
+            //4.The application receives an X number of inputs.
+            //It must return the correspondent value on request,
+            //on after each request it must ask the user if it desires
+            //another retrieval.The information must be stored in an array;
+            //int size = 0;
+            //Console.WriteLine("4.X numbers:");
+            //int[] arrayNumberFour = new int[size];
+            //for (int k = 0; k < size; k++)
+            //{
+            //    arrayNumberFour[k] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("number of the position on array");
+            
+
+
+
+
+            //5.Create the tic tac toe game.The data must be
+            //stored in an array and the request played position is
+            //provided by the user with two coordinates.The application
+            //must validate it the user play is valid;
+
+           // TicTac.ticTacToe();
+            Tic.PlayXorO();
+
+        }
+    }
+}
+    
+
+         
---
+++ 5.Arrays/tictactoe/tictactoe/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace tictactoe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char player = 'X';
+            char[,] board = new char[3, 3];
+            bool valid = true;
+            inicialBoard(board);
+            int movesDraw = 0;
+
+         
+
+            while (true)
+            {
+                Console.Clear();
+                Print(board);
+
+                Console.Write(" please enter row: ");
+                int row = Convert.ToInt32(Console.ReadLine());
+                Console.Write("please enter col: ");
+                int col = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if (board[row,col] == ' ')
+                {
+                    board[row, col] = player;
+                    movesDraw = movesDraw + 1;
+                    player = Player(player);
+                }
+                else
+                {
+                    Console.WriteLine(" choose another");
+                    Console.ReadKey();
+                }
+
+                if(player == board[0, 0] && player == board[0,1] && player == board[0,2] || 
+                    player == board[1, 0] && player == board[1, 1] && player == board[1, 2] || 
+                    player == board[2, 0] && player == board[2, 1] && player == board[2, 2])
+                {
+                    Console.WriteLine("player " + player + " win" );
+                    break;
+                }
+                else if (player == board[0, 0] && player == board[1, 0] && player == board[2, 0] ||
+                    player == board[1, 0] && player == board[1, 1] && player == board[1, 2] ||
+                    player == board[2, 0] && player == board[1, 1] && player == board[2, 2])
+                {
+                    Console.WriteLine("player " + player + " win");
+                    break;
+                }
+                else if (player == board[0, 0] && player == board[1, 1] && player == board[2, 2] ||
+                    player == board[2, 0] && player == board[1, 1] && player == board[0, 2])
+                {
+                    Console.WriteLine("player " + player + " win");
+                    break;
+                }
+
+
+                if (movesDraw == 9)
+                {
+                    Console.WriteLine(" draw");
+                    break;
+                }
+
+            }
+        }
+        static void inicialBoard(char[,] board)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+
+
+        }
+        static void Print(char[,] board)
+        {
+            Console.WriteLine("  | 0 | 1 | 2 |");
+            for (int row = 0; row < 3; row++)
+            {
+                Console.Write(row +" | ");
+               
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(board[row, col]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static char Player(char currentPlayer)
+        {
+                if (currentPlayer == 'X')
+                {
+                    return  'O';   
+                }
+                else
+                {
+                    return 'X';
+                }
+        }
+
+        
+  
+    }
+}
